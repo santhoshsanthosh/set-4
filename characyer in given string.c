6aaff03476ce2db803e0e65ba141260dbfb8ce3c
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-01-23 11:22:30 +0000

    Update characyer in given string.c

--- characyer in given string.c
+++ characyer in given string.c
@@ -1,14 +1,22 @@
-#include<iostream>
+#include<stdio.h>
+#include<conio.h>
 #include<string.h>
-void main()
+int main()
 {
-char c[30]="its me gj.44";
-int i,s=0;
-for(i=0;c[i]!=0;i++)
+    char a[50];
+    int j,count=0,c;
+    printf("enter the string");
+    gets(a);
+    c=strlen(a);
+    for(j=0;j<=c;j++)
+    {
+    if(33<=a[j]<=47||58<=a[j]<=64||91<=a[j]<=96||123<=a[j]<=126)
 {
-	if((c[i]>='1')&&(c[i]<='9'))
-	s++;
+     count=count+1;
+ }
+   }
+      printf("the total no of special characters in string is %d",count);
+      return 0;
+ }
 
-}
- cout<<s;
-}
+
