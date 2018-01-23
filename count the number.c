3cfe14c79d34d89b3c25de295c35826251442411
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-01-23 10:42:05 +0000

    Update count the number.c

--- count the number.c
+++ count the number.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
+
 int main()
 {
+  
   char s[222];
+  
   int count=0;
+  
   printf("Enter the string\n");
+  
   scanf("%[^\n]s",s);
+  
   for(i=0;s[i]!='\0';i++)
 {
-  if(s[i]==' ')
-  ount++;
+ 
+    if(s[i]==)
+    count++;
 }
-  printf("Number of words in given string are:%d\n",count+1);
-  return0;
+    printf("Number of words in given string are:%d\n",count+1);
+ 
+    return0;
 }
