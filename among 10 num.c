780dc4db6311b7c3dc92749bb330447c4d67aa7d
santhoshsanthosh <31506624+santhoshsanthosh@users.noreply.github.com>
2018-02-01 14:35:29 +0530

    Create among 10 num.c

---
+++ among 10 num.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+ {
+   int a[20],i,l;
+   for(i=0;i<10;i++)
+   
+   {
+     scanf("%d",&a[i]);
+   }
+     l=a[0];
+     for(i=0;i<10;i++)
+   {
+     if(l<a[i])
+      {
+        [i];
+      }
+    }
+     printf("%d",l);
+  }
+     return 0;
+     getch()
+     
